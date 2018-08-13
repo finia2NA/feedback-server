7b23e55e6b57014e1ecd4e342b2e052363725128
finia2NA <undifini@gmail.com>
2018-08-13 17:59:57 +0200

    Analyzer Beginning / Print IP on Startup

---
+++ src/Analyzer.java
@@ -0,0 +1,22 @@
+import acm.program.GraphicsProgram;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+public class Analyzer extends GraphicsProgram {
+
+  public void init() {
+    DatagramSocket socket;
+    try {
+      socket = new DatagramSocket();
+      socket.connect(InetAddress.getByName("8.8.8.8"), 10002);
+      String ip = socket.getLocalAddress().getHostAddress();
+      println("Local IP is " + ip);
+    } catch (SocketException | UnknownHostException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+  }
+
+}
