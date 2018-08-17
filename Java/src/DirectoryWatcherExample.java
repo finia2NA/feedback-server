c533d5be3cbea655619b8493aaf79c1ad0f2c40f
Finite <33845166+finia2NA@users.noreply.github.com>
2018-08-17 16:22:18 +0200

    ACMified the Watcher and added ip printing code

--- Java/src/DirectoryWatcherExample.java
+++ Java/src/DirectoryWatcherExample.java
@@ -1,3 +1,7 @@
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -6,13 +10,29 @@ import java.nio.file.WatchEvent;
 import java.nio.file.WatchKey;
 import java.nio.file.WatchService;
 
-public class DirectoryWatcherExample {
+import acm.program.GraphicsProgram;
 
-  public static void main(String[] args) {
+@SuppressWarnings("serial")
+public class DirectoryWatcherExample extends GraphicsProgram {
+
+  final static Path path = Paths.get("O:\\Studium\\Kompetenz\\feedback-server\\Node\\data");
+  WatchService watchService;
+
+  public void init() {
+    try {
+      watchService = FileSystems.getDefault().newWatchService();
+      path.register(watchService, StandardWatchEventKinds.ENTRY_CREATE, StandardWatchEventKinds.ENTRY_DELETE,
+          StandardWatchEventKinds.ENTRY_MODIFY);
+      println("Expecting data in: " + path);
+      printIP();
+    } catch (Exception e) {
+    }
+    // readLine("Press Enter to start");
+  }
+
+  public void run() {
     try {
       WatchService watchService = FileSystems.getDefault().newWatchService();
-
-      Path path = Paths.get("O:\\Studium\\Kompetenz\\feedback-server\\Node\\data");
 
       path.register(watchService, StandardWatchEventKinds.ENTRY_CREATE, StandardWatchEventKinds.ENTRY_DELETE,
           StandardWatchEventKinds.ENTRY_MODIFY);
@@ -29,6 +49,14 @@ public class DirectoryWatcherExample {
     Exception e) {
 
     }
+  }
+
+  private void printIP() throws SocketException, UnknownHostException {
+    DatagramSocket socket;
+    socket = new DatagramSocket();
+    socket.connect(InetAddress.getByName("8.8.8.8"), 10002);
+    String ip = socket.getLocalAddress().getHostAddress();
+    println("Local IP is " + ip);
   }
 
 }
