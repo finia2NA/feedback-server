4df7a743121143b71f64bdd33db7ea0b9b955505
finia2NA <undifini@gmail.com>
2018-08-22 21:14:53 +0200

    MainView beginning

---
+++ Java/src/View/MainView.java
@@ -0,0 +1,11 @@
+package View;
+
+import acm.program.GraphicsProgram;
+
+public class MainView {
+  GraphicsProgram top;
+
+  public MainView(GraphicsProgram top, int numberOfAreas) {
+    this.top = top;
+  }
+}
